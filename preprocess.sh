dd566d3952aa6a045be8a67a0a71b8d264a0deeb
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2022-06-01 08:55:25 +0000

    シェルスクリプトの途中でエラーが発生した場合，その時点で終了するようにする
    (cherry picked from commit aa9860d42a96b36731749c0c873f026f863f3696)

--- preprocess.sh
+++ preprocess.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -eu
 ###########################################################
 # Change the following values to preprocess a new dataset.
 # TRAIN_DIR, VAL_DIR and TEST_DIR should be paths to      
