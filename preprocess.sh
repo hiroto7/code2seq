48e9d4602bf55716ddc92c6e10f5f8f2e43e39c2
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2022-10-13 15:29:53 +0900

    Modify `TRAIN_DIR`, `VAL_DIR`, `TEST_DIR`

--- preprocess.sh
+++ preprocess.sh
@@ -21,9 +21,9 @@
 #   recommended to use a multi-core machine for the preprocessing 
 #   step and set this value to the number of cores.
 # PYTHON - python3 interpreter alias.
-TRAIN_DIR=../data/raw
-VAL_DIR=../data/raw
-TEST_DIR=../data/raw
+TRAIN_DIR=../data/raw/train
+VAL_DIR=../data/raw/val
+TEST_DIR=../data/raw/test
 DATASET_NAME=path_contexts
 MAX_DATA_CONTEXTS=1000
 MAX_CONTEXTS=200
